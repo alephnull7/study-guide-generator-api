8b71f437e3804a5314f07908cb90b8ba66556f41
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-04-02 15:02:02 -0700

    Deduplicate service code

--- services/userService.js
+++ services/userService.js
@@ -1,43 +1,26 @@
 const UserModel = require('../models/userModel');
+const serviceModel = require("./helpers/helpers");
 const userModel = new UserModel();
 
 class UserService {
     async createUser(userData) {
-        const postKeys = Object.keys(userData);
         const neededFields = ['email', 'account_type'];
-        for (let field of neededFields) {
-            if (!postKeys.includes(field)) {
-                return 0;
-            }
-        }
-        return await userModel.createUser(userData);
+        return await serviceModel(userData, neededFields, userModel.createUser(userData));
     }
 
     async getUser(userData) {
-        const getKeys = Object.keys(userData);
-        if (!getKeys.includes('id')) {
-            return 0;
-        }
-        return await userModel.getUser(userData);
+        const neededFields = ['id'];
+        return await serviceModel(userData, neededFields, userModel.getUser(userData));
     }
 
     async updateUser(userData) {
-        const putKeys = Object.keys(userData);
         const neededFields = ['email', 'id'];
-        for (let field of neededFields) {
-            if (!putKeys.includes(field)) {
-                return 0;
-            }
-        }
-        return await userModel.updateUser(userData);
+        return await serviceModel(userData, neededFields, userModel.updateUser(userData));
     }
 
     async deleteUser(userData) {
-        const deleteKeys = Object.keys(userData);
-        if (!deleteKeys.includes('id')) {
-            return 0;
-        }
-        return await userModel.deleteUser(userData);
+        const neededFields = ['id'];
+        return await serviceModel(userData, neededFields, userModel.deleteUser(userData));
     }
 }
 
