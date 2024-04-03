c5cedd832fe704093d8be44aa934c818cdd9241c
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-04-02 19:28:39 -0700

    Origination of `classroom` API

---
+++ models/classroomModel.js
@@ -0,0 +1,114 @@
+const pool = require('../db/db');
+
+class ClassroomModel {
+    constructor() {
+        this.tableName = 'classroom';
+    }
+
+    async createClassroom(classroomData) {
+        console.log(classroomData)
+        const query =
+            `INSERT INTO ${this.tableName} (name, instructed) VALUES ($1, $2) RETURNING *`;
+        console.log(query);
+        const values = [classroomData.name, classroomData.user_id];
+        const { rows } = await pool.query(query, values);
+        return rows[0];
+    }
+
+    async getClassroom(classroomData) {
+        const query =
+            `SELECT * FROM ${this.tableName} WHERE _id = $1`;
+        console.log(query);
+        const values = [classroomData.id];
+        const { rows } = await pool.query(query, values);
+        if (rows.length === 0) {
+            return 1;
+        }
+        return rows[0];
+    }
+
+
+    async addToClassroom(classroomData) {
+        const students = classroomData.students;
+        const id = classroomData.id;
+        let valuesStr = '';
+        let values = [];
+        let placeholder = 1;
+        for (let i = 0; i < students.length; i ++) {
+            values.push(students[i]);
+            values.push(id);
+            valuesStr += `($${placeholder}, $${placeholder+1})`;
+            if (i < students.length-1) {
+                valuesStr += ', ';
+            }
+            placeholder += 2;
+        }
+        const query =
+            `INSERT INTO classroom_student (student_id, classroom_id) VALUES ${valuesStr} RETURNING *`;
+        console.log(query);
+        const { rows } = await pool.query(query, values);
+        if (rows.length === 0) {
+            return 1;
+        }
+        return rows;
+    }
+
+    async removeFromClassroom(classroomData) {
+        const students = classroomData.students;
+        let valuesStr = '';
+        for (let i = 1; i <= students.length; i ++) {
+            valuesStr += `$${i+1}`;
+            if (i < students.length) {
+                valuesStr += ', ';
+            }
+        }
+        const query =
+            `DELETE from classroom_student WHERE classroom_id = $1 AND student_id IN (${valuesStr}) RETURNING *`;
+        console.log(query);
+        const values = [classroomData.id, ...classroomData.students];
+        const { rows } = await pool.query(query, values);
+        if (rows.length === 0) {
+            return 1;
+        }
+        return rows;
+    }
+
+    async assignToClassroom(classroomData) {
+        const artifacts = classroomData.artifacts;
+        const id = classroomData.id;
+        let valuesStr = '';
+        let values = [];
+        let placeholder = 1;
+        for (let i = 0; i < artifacts.length; i ++) {
+            values.push(artifacts[i]);
+            values.push(id);
+            valuesStr += `($${placeholder}, $${placeholder+1})`;
+            if (i < artifacts.length-1) {
+                valuesStr += ', ';
+            }
+            placeholder += 2;
+        }
+        const query =
+            `INSERT INTO classroom_artifact (artifact_id, classroom_id) VALUES ${valuesStr} RETURNING *`;
+        console.log(query);
+        const { rows } = await pool.query(query, values);
+        if (rows.length === 0) {
+            return 1;
+        }
+        return rows;
+    }
+
+    async deleteClassroom(classroomData) {
+        const query =
+            `DELETE FROM ${this.tableName} WHERE _id = $1 RETURNING *`;
+        console.log(query);
+        const values = [classroomData.id];
+        const { rows } = await pool.query(query, values);
+        if (rows.length === 0) {
+            return 1;
+        }
+        return rows[0];
+    }
+}
+
+module.exports = ClassroomModel;
