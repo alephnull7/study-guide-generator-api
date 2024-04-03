2100273c0dafc4e43ae14acdb24bd6966f3bfef1
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-04-03 12:53:11 -0700

    Cross-origin support

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 
+// Allow requests from all origins
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 // Routes
 app.use('/api/users', userController); // Mount user routes under '/api/users'
 app.use('/api/artifacts', artifactController);
