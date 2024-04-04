021328c12d7154e28842b60958843200855efecc
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-04-04 13:56:14 -0700

    Success codes relate to request

--- controllers/helpers/helpers.js
+++ controllers/helpers/helpers.js
@@ -1,4 +1,4 @@
-function relayResponse(res, result) {
+function relayResponse(req, res, result) {
     switch (result) {
         case 0:
             res.status(400).json({ message: 'Bad Request' });
@@ -7,14 +7,24 @@ function relayResponse(res, result) {
             res.status(204).send();
             break;
         default:
-            res.status(201).json(result);
+            const successCode = getSuccessCode(req.method);
+            res.status(successCode).json(result);
+    }
+}
+
+function getSuccessCode(method) {
+    switch (method) {
+        case 'POST':
+            return 201;
+        default:
+            return 200;
     }
 }
 
 async function controlService(req, res, serviceRequest) {
     try {
         let result = await serviceRequest;
-        relayResponse(res, result);
+        relayResponse(req, res, result);
     } catch (error) {
         console.error(error);
         res.status(500).json({message: 'Internal Server Error'});
