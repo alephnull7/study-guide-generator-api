cec0a2bc5c2642fecd1a12547f2b36134e68fce0
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-04-04 19:14:11 -0700

    models module testing

---
+++ test/models/classroomModel.test.js
@@ -0,0 +1,55 @@
+const ClassroomModel = require('../../models/classroomModel');
+
+describe("getClassrooms", () => {
+    it("should return the classroom the user with the given userId", async () => {
+        const classroomModel = new ClassroomModel();
+        const requestObj = { id: 6 };
+        const response = await classroomModel.getClassrooms(requestObj);
+        expect(response.length).toEqual(5);
+    });
+});
+
+describe("handleClassroom", () => {
+    it("should create classroom", async () => {
+        const classroomModel = new ClassroomModel();
+        const requestObj = {
+            name: 'test',
+            user_id: 6
+        };
+
+        // create
+        const classroomObj = await classroomModel.createClassroom(requestObj);
+        expect(classroomObj.instructed).toEqual(requestObj.user_id);
+
+       // delete
+        requestObj.id = classroomObj._id;
+        const response = await classroomModel.deleteClassroom(requestObj);
+        expect(response._id).toEqual(classroomObj._id);
+    });
+});
+
+describe("handleStudents", () => {
+    it("should add and remove students from classroom", async () => {
+        const classroomModel = new ClassroomModel();
+        const requestObj = {
+            id: 4,
+            students: [9,39]
+        };
+        let response = await classroomModel.addToClassroom(requestObj);
+        expect(response.length).toEqual(2);
+        response = await classroomModel.removeFromClassroom(requestObj);
+        expect(response.length).toEqual(2);
+    }, 10000);
+});
+
+describe("assignArtifacts", () => {
+    it("should add and remove students from classroom", async () => {
+        const classroomModel = new ClassroomModel();
+        const requestObj = {
+            id: 4,
+            artifacts: [2]
+        };
+        const response = await classroomModel.assignToClassroom(requestObj);
+        expect(response.length).toEqual(1);
+    }, 10000);
+});
